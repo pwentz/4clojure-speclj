3bb58d05e363815f881b0231576142d534a1ceb6
Kevin Buchanan <kevaustinbuch@gmail.com>
2015-05-25 15:29:34 -0500

    Upgrade clojure

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject fourclojure "0.1.0-SNAPSHOT"
+(defproject fourclojure "0.1.1"
   :description "4clojure problems using speclj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:dev {:dependencies [[speclj "3.0.1"]]}}
-  :plugins [[speclj "3.0.1"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {:dev {:dependencies [[speclj "3.2.0"]]}}
+  :plugins [[speclj "3.2.0"]]
   :test-paths ["spec"])
